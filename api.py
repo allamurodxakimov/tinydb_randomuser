7d4b4ad84dd43c57d8a3517750f1e08a9a9f3e25
allamurodxakimov <xakimov2806@gmail.com>
2023-12-21 16:53:03 +0300

    randomuser

--- api.py
+++ api.py
@@ -1,3 +1,33 @@
 import requests
+from tinydb import TinyDB
 
 base_url = "https://randomuser.me/api/"
+db = TinyDB("data.json",indent = 4)
+def get_users(gender: str, n: int):
+    payload = {
+        "results":n,
+        "gender":gender,
+    }
+    respons = requests.get(base_url, params=payload)
+
+    if respons.status_code==200:
+        users = []
+        for i in respons.json()['results']:
+            users.append({
+                "first_name":i['name']['first'],
+                "last_name":i['name']['last'],
+                "age":i['dob']['age'],
+                "phone":i['phone'],
+                "country":i['location']['country'],
+                "email":i['email'],
+            })
+        
+        return users
+    else:
+        return []
+    
+male = db.table("males")
+female = db.table("females")
+male.insert_multiple(get_users('male',3000))
+female.insert_multiple(get_users('female',3000))
+
--- main.py
+++ main.py
@@ -1,3 +1,53 @@
 from tinydb import TinyDB
+from tinydb.database import Document
+from api import url
+import json
+db = TinyDB('data.json',indent = 9)
+def update():
+    data = url()
+    if (len(data))!=0:
+        return data['results'][0]
+    return False
+def get_user(data):
+    data = update()
+    if data!=False:
+        users = {
+            "gender":data['gender'],
+            "first_name":data['name']['first'],
+            "last_name":data['name']['last'],
+            "age":data['dob']['age'],
+            "phone":data['phone'],
+            "country":data['location']['country'],
+            "email":data['email'],
 
-db = TinyDB('data.json')
+        }
+        return users
+    else:
+        return False
+
+def add_user(data, quantity,gender1,gender2):
+    a = 0
+    b = 0
+    while quantity!=a:
+        
+        get_data = get_user(data)
+        if get_data["gender"] == gender1:
+            users = db.table("users_male")
+            
+            users.insert(get_data)
+            a+=1
+    while quantity!=b:
+        get_data = get_user(data)
+        if get_data['gender'] ==gender2:
+            users = db.table("users_female")
+
+            users.insert(get_data)
+            b+=1
+    users.all()
+
+
+    return "Accept"
+    
+data = update()
+
+print(add_user(data,30,gender1="male",gender2="female"))
